320daedd2de7823249a5abc62c9a7eafb2bbb779
Craig Ryan <s3555490@student.rmit.edu.au>
2021-03-31 09:01:46 +1030

    Started email notification

---
+++ JobSpotAplication/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSpotAplication.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SenderGridUser { get; set; }
+        public string SenderGridKey { get; set; }
+    }
+}
