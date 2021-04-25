bdbbd77c39df9a69b1c59edad887b6dcc6ebbcc0
Craig Ryan <s3555490@student.rmit.edu.au>
2021-04-25 10:44:04 +0930

    Key vault issue

--- JobSpotAplication/Program.cs
+++ JobSpotAplication/Program.cs
@@ -17,7 +17,23 @@ namespace JobSpotAplication
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Build the current set of configuration to load values from
+                    // JSON files and environment variables, including VaultName.
+                    var builtConfig = config.Build();
+
+                    // Use VaultName from the configuration to create the full vault URL.
+                    var vaultName = builtConfig["jobspot"];
+                    Uri vaultUri = new Uri($"https://jobspot.vault.azure.net/");
+
+                    // Load all secrets from the vault into configuration. This will automatically
+                    // authenticate to the vault using a managed identity. If a managed identity
+                    // is not available, it will check if Visual Studio and/or the Azure CLI are
+                    // installed locally and see if they are configured with credentials that can
+                    // access the vault.
+                    config.AddAzureKeyVault(vaultUri, new Azure.Identity.DefaultAzureCredential());
                 });
-               
     }
 }
