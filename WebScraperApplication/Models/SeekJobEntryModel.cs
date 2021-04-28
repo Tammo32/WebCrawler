5aac4c03cb6c43cc99070009c6b92a4e7ee621b4
Thomas Kauran <tkauran@gmail.com>
2021-04-28 18:34:37 +1000

    Added further seek web scraping

---
+++ WebScraperApplication/Models/SeekJobEntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper.Models
+{
+	public class SeekJobEntryModel : JobEntryModel
+	{
+
+		public SeekJobEntryModel(string title, string company, string type, string description)
+		{
+			Title = title;
+			Company = company;
+			Description = description;
+			Type = type;
+		}
+	}
+}
