eb0c64123e1189eab5dd6efd4ad1f6a2e49e06fc
Thomas Kauran <tkauran@gmail.com>
2021-05-01 23:31:30 +1000

    Added functionality of searching for job and displaying the data in a view for seek jobs to Dashboard

--- JobSpotAplication/Controllers/DashboardController.cs
+++ JobSpotAplication/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using WebScraper;
+using WebScraper.Models;
 using WebScraper.WebScraper;
 
 namespace JobSpotAplication.Controllers
@@ -28,7 +29,27 @@ namespace JobSpotAplication.Controllers
 		public IActionResult JobSearch(string title, string location, string availability, string daterange, string startingPayRange, string endingPayRange, string salaryType)
 		{
 			ISeekWebScraper seekWebScraper = new SeekWebScraperModel();
-			Dictionary<string, string> searchParams = new Dictionary<string, string>()
+			Dictionary<string, string> searchParams = GetSeekUrl(title, location, availability, daterange, startingPayRange, endingPayRange, salaryType);
+			string url = seekWebScraper.BuildUrl(searchParams);
+			List<JobEntryModel> seekJobs = GetJobsBySearch(url, searchParams, seekWebScraper);
+			ViewData["seekJobs"] = seekJobs;
+			return View("Index", seekJobs);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		private List<JobEntryModel> GetJobsBySearch(string url, Dictionary<string, string> searchParams, ISeekWebScraper scraper)
+		{
+			return scraper.ScrapeMultipleJobs(url, searchParams);
+		}
+
+		private Dictionary<string, string> GetSeekUrl(string title, string location, string availability, string daterange, string startingPayRange, string endingPayRange, string salaryType)
+		{
+			return new Dictionary<string, string>()
 			{
 				["title"] = title,
 				["location"] = location,
@@ -38,14 +59,6 @@ namespace JobSpotAplication.Controllers
 				["endingPayRange"] = endingPayRange,
 				["salaryType"] = salaryType
 			};
-			ViewData["url"] = seekWebScraper.BuildUrl(searchParams);
-			return View("Index");
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
 }
