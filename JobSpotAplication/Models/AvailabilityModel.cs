47387bd6f6ec0956d20288de22576b720291ddd1
Thomas Kauran <tkauran@gmail.com>
2021-05-06 23:15:48 +1000

    Created models for Availability, Daterange and SalaryRange. Dynamically displaying those sets of data to job search form now in view job search view partial.

---
+++ JobSpotAplication/Models/AvailabilityModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSpotAplication.Models
+{
+	public static class AvailabilityModel
+	{
+		[DisplayName("Full TIme")]
+		public static string FullTime { get { return "full-time"; } }
+		[DisplayName("Part Time")]
+		public static string PartTime { get { return "part-time"; } }
+		[DisplayName("Contract / Temp")]
+		public static string Contract { get { return "contract-temp"; } }
+		[DisplayName("Casual / Vacation")]
+		public static string Casual { get { return "casual-vacation"; } }
+		public static List<string> AvailabilityList { get; private set; } = new List<string>() 
+		{ FullTime, PartTime, Contract, Casual };
+	}
+}
