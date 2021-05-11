bf6a9a6b3e8ee7fba1c59813ac63b366775d076a
Craig Ryan <s3555490@student.rmit.edu.au>
2021-05-11 12:58:31 +0930

    Added admin controller and veiws

---
+++ JobSpotAplication/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using JobSpotAplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSpotAplication.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext DbConext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- JobSpotAplication/Controllers/HomeController.cs
+++ JobSpotAplication/Controllers/HomeController.cs
@@ -1,13 +1,9 @@
 ﻿using JobSpotAplication.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
+
 
 namespace JobSpotAplication.Controllers
 {
