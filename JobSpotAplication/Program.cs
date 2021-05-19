94050a7d534291dda57e98686b7b6bbd953b761e
Craig Ryan <s3555490@student.rmit.edu.au>
2021-05-19 23:13:49 +0930

    changed connection strings

--- JobSpotAplication/Program.cs
+++ JobSpotAplication/Program.cs
@@ -34,14 +34,14 @@ namespace JobSpotAplication
 
                      // Use VaultName from the configuration to create the full vault URL.
                      var vaultName = builtConfig["jobspot"];
-                    Uri vaultUri = new Uri($"https://jobspot.vault.azure.net/");
+                     Uri vaultUri = new Uri($"https://jobspot.vault.azure.net/");
 
                      // Load all secrets from the vault into configuration. This will automatically
                      // authenticate to the vault using a managed identity. If a managed identity
                      // is not available, it will check if Visual Studio and/or the Azure CLI are
                      // installed locally and see if they are configured with credentials that can
                      // access the vault.
-                    config.AddAzureKeyVault(vaultUri, new Azure.Identity.DefaultAzureCredential());
+                     config.AddAzureKeyVault(vaultUri, new Azure.Identity.DefaultAzureCredential());
                  });
     }
 }
