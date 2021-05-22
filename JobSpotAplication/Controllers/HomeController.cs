fa6dc1ce5c0dfa2f772cee26147ee97e6c4e6d5e
Craig Ryan <s3555490@student.rmit.edu.au>
2021-05-22 21:06:28 +0930

    update to redirect to dash on login

--- JobSpotAplication/Controllers/HomeController.cs
+++ JobSpotAplication/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace JobSpotAplication.Controllers
@@ -25,6 +26,16 @@ namespace JobSpotAplication.Controllers
 
         public IActionResult Privacy()
         {
+            string userId = User.FindFirstValue(ClaimTypes.Name);
+
+            if (userId == "admin@admin")
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            if(userId != null)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
             return View();
         }
 
