e45fdd0f9fac0394f753941313b86d135585c031
s3749078 <s3749078@student.rmit.edu.au>
2021-05-22 21:15:07 +1000

    Created About view, added cards to About view. Further CSS changes. Added placeholder images.

--- JobSpotAplication/wwwroot/js/site.js
+++ JobSpotAplication/wwwroot/js/site.js
@@ -9,3 +9,16 @@ $('#JobSearch').on('click', function (e) {
         $('#replace').html(html);
     });
 });
+
+/* Test for Active Menu Items
+var sidebar = document.getElementById("sidebar-nav");
+var options = document.getElementByClassName("sidebar-option");
+
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function ()) {
+        var current = document.getElementByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+    });
+}
+*/
