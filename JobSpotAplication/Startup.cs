510650d057d256dd944a32ec4b31884c4db9c176
Craig Ryan <s3555490@student.rmit.edu.au>
2021-05-22 20:00:00 +0930

    dev mode

--- JobSpotAplication/Startup.cs
+++ JobSpotAplication/Startup.cs
@@ -91,9 +91,11 @@ namespace JobSpotAplication
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
